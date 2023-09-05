b636a4fc649f95d1a57b72fc3939f37f66f2502f
lmfae <achen10@su.suffolk.edu>
2023-09-05 19:35:18 -0400

    some random message

--- client3.py
+++ client3.py
@@ -29,31 +29,51 @@ QUERY = "http://localhost:8080/query?id={}"
 N = 500
 
 
-def getDataPoint(quote):
-    """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
-    stock = quote['stock']
-    bid_price = float(quote['top_bid']['price'])
-    ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+def getDataPoint(quote: dict) -> tuple:
+    """Produce all the needed values to generate a datapoint
+
+    Args:
+        quote (dict): a dictionary representing a stock quote
+    Returns:
+        tuple: a tuple of values"""
+    # getDataPoint function should return correct tuple of stock name, bid_price, ask_price and price. Note: price of a stock = average of bid and ask
+
+    stock = quote["stock"]
+    bid_price = float(quote["top_bid"]["price"])
+    ask_price = float(quote["top_ask"]["price"])
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
-def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+def getRatio(price_a: float, price_b: float) -> float:
+    """Get ratio of price_a and price_b
+
+    Args:
+        price_a: float value of price a
+
+        price_b: float value of price b
+    Returns:
+        float: a ratio of price a and price b
+    """
+    # getRatio function should return the ratio of the two stock prices
+    return price_a / price_b
 
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
     for _ in iter(range(N)):
-        quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+        quotes = json.loads(
+            urllib.request.urlopen(QUERY.format(random.random())).read()
+        )
 
-        """ ----------- Update to get the ratio --------------- """
+        ratio_dict: dict = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
-
-        print("Ratio %s" % getRatio(price, price))
+            ratio_dict[stock] = price
+            print(
+                "Quoted %s at (bid:%s, ask:%s, price:%s)"
+                % (stock, bid_price, ask_price, price)
+            )
+        # print(ratio_dict)
+        print("Ratio %s" % getRatio(ratio_dict["ABC"], ratio_dict["DEF"]))
